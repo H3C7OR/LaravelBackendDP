7cb5cca86b1bcad2eef050fad02451f3ab17e598
Hector P <hrpo.17@gmail.com>
2019-04-01 11:48:14 -0400

    3. Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+    // return view('welcome');
+}); 
+
+Route::get('/usuarios', function(){
+    return 'Usuarios';
 });
 
+Route::get('/usuarios/{id}', function($id) {
+    return "Mostrando detalle del usuario: {$id}";
+})->where('id', '[0-9]+');
+
+Route::get('/usuarios/nuevo', function() {
+    return "Crear nuevo usuario";
+});
+
+Route::get('/saludo/{name}/{nickname?}', function($name, $nickname = null){
+    if ($nickname != null){
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    }        
+    else{
+        return "Bienvenido {$name}";
+    }    
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/timeline', function() {
-    return 'Timeline';
-});
