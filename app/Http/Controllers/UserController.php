e0985b7c2256956980792dcc645fbc53865f085f
Hector P <hrpo.17@gmail.com>
2019-04-01 16:22:13 -0400

    6. Creation and use of views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,30 @@ class UserController extends Controller
 {
     public function index()
     {
-        return 'Usuarios';
+        $users = [
+            'Joel',
+            'Ellie',
+            'Tess',
+            'Tommy',
+            'Bill',
+            '<script>alert("Clicker")</script>'
+        ];
+        
+
+        /* return view('users', [
+            'users' => $users,
+            'title' => 'Listado de usuarios'
+        ]); */
+        /* return view('users')->with([
+            'users' => $users,
+            'title' => 'Listado de usuarios'
+        ]); */
+        /* return view('users')
+            ->with('users', $users)
+            ->with('title', 'Listado de usuarios'); */
+        $title = 'Listado de usuarios';
+
+        return view('users', compact('title', 'users'));
     }
 
     public function show($id)
--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -13,7 +13,10 @@ class UsersModuleTest extends TestCase
     {
         $this->get('/usuarios')
             ->assertStatus(200)
-            ->assertSee('Usuarios');
+            ->assertSee('Listado de usuarios')
+            ->assertSee('Joel')
+            ->assertSee('Ellie');
+
     }
     
     /** @test  */
